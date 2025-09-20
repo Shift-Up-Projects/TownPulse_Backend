49709c8a1ea16eb765ce7a6c41b2429591ccc1ab
RazanAlhasan <razanalhasan51@gmail.com>
2025-09-20 13:55:43 +0300

    add utils folder with helper functions

---
+++ utils/enum.js
@@ -0,0 +1,13 @@
+exports.UserStatus = {
+  active: 'active',
+  disactive: 'disactive',
+};
+exports.RoleCode = {
+  USER: 'USER',
+  ADMIN: 'ADMIN',
+};
+exports.Env = {
+  production: 'production',
+  development: 'development',
+  test: 'test',
+};
