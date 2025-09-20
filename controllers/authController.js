722305f8d48b1169f6415043597e67b6f7ae25b0
RazanAlhasan <razanalhasan51@gmail.com>
2025-09-20 19:50:56 +0300

    add model/controller/router for user

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,29 +69,31 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   if (!user) {
     return next(new AppError('There is no user with email address.', 404));
   }
-  
+
   // 2) Generate the random reset token
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
-  
+
   // 3) Send it to user's email
   try {
     const resetURL = `${req.protocol}://${req.get('host')}/api/v1.0.0/users/resetPassword/${resetToken}`;
- 
+
     await new Email(user, resetURL).sendPasswordReset();
-    
+
     res.status(200).json({
       status: 'success',
-      message: 'Token sent to email!'
+      message: 'Token sent to email!',
     });
-    
   } catch (err) {
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
     await user.save({ validateBeforeSave: false });
-    
+
     return next(
-      new AppError('There was an error sending the email. Try again later!', 500)
+      new AppError(
+        'There was an error sending the email. Try again later!',
+        500,
+      ),
     );
   }
 });
---
+++ routes/userRouter.js
@@ -0,0 +1,73 @@
+const express = require('express');
+// const userController = require('./../controllers/userController');
+const authController = require('../controllers/authController');
+const authMiddlewers = require('../middlewares/authMiddlewers');
+const imguserMiddlewers = require('../middlewares/imguserMiddlewers');
+const { protect, restrictTo } = require('../middlewares/authMiddlewers');
+const router = express.Router();
+
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+router.get('/resetPassword/:token', (req, res) => {
+  res.render('user/resetPassword4');
+});
+router.post('/signup', authController.signup);
+// عمليات المستخدم على حسابه الخاص
+// router.patch('/activeMe', authMiddlewers.protect, userController.activeMe);
+router.patch(
+  '/updateMyPassword',
+  authMiddlewers.protect,
+  authController.updatePassword,
+);
+// router.get(
+//   '/me',
+//   authMiddlewers.protect,
+//   userController.getMe,
+//   userController.getUser,
+// );
+// router.patch(
+//   '/updateMeAndUpload',
+//   authMiddlewers.protect,
+//   imguserMiddlewers.uploadUserPhoto,
+//   userController.updateMe,
+// );
+// router.patch('/updateMe', authMiddlewers.protect, userController.updateMe);
+// router.delete('/deleteMe', authMiddlewers.protect, userController.deleteMe);
+// // طرق ادارة المستخدمين
+// router
+//   .route('/')
+//   .get(
+//     authMiddlewers.protect,
+//     authMiddlewers.isactive,
+//     authMiddlewers.restrictTo('ADMIN'),
+//     userController.getAllUsers, //,الحصول على جميع المستخدمين
+//   )
+//   .post(
+//     authMiddlewers.protect,
+//     authMiddlewers.isactive,
+//     authMiddlewers.restrictTo('ADMIN'),
+//     userController.createUser, //انشاء مستخدم جديد
+//   );
+// router
+//   .route('/:id')
+//   .get(
+//     authMiddlewers.protect,
+//     authMiddlewers.isactive,
+//     authMiddlewers.restrictTo('ADMIN'),
+//     userController.getUser, //الحصول على مستخدم معين
+//   )
+//   .patch(
+//     authMiddlewers.protect,
+//     authMiddlewers.isactive,
+//     authMiddlewers.restrictTo('ADMIN'),
+//     userController.updateUser, //تحديث مستخدم معين
+//   )
+//   .delete(
+//     authMiddlewers.protect,
+//     authMiddlewers.isactive,
+//     authMiddlewers.restrictTo('ADMIN'),
+//     userController.deleteUser, //حذف مستخدم معين
+//   );
+module.exports = router;
