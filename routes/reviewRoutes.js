a59deb10ba0b94e365c7fb049e3b62db9a5398f6
RazanAlhasan <razanalhasan51@gmail.com>
2025-10-06 19:51:07 +0300

    add review routes with auth and validation

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const reviewController = require('../controllers/reviewController');
+const authMiddlewers = require('../middlewares/authMiddlewers');
+const reviewMiddleware = require('../middlewares/reviewMiddleware');
+router.use(authMiddlewers.protect);
+
+router.post(
+  '/',
+  reviewMiddleware.validateCreateReview,
+  reviewController.createReview
+);
+router.get(
+  '/',
+  reviewMiddleware.validateQueryParams,
+  reviewController.getAllReviews
+);
+router.get(
+  '/:id',
+  reviewMiddleware.validateReviewId,
+  reviewController.getOneReview
+);
+router.patch(
+  '/:id',
+  reviewMiddleware.validateReviewId,
+  reviewMiddleware.checkReviewOwner,
+  reviewMiddleware.validateUpdateReview,
+  reviewController.updateReview
+);
+router.delete(
+  '/:id',
+  reviewMiddleware.validateReviewId,
+  reviewMiddleware.checkReviewOwner,
+  reviewController.deleteReview
+);
+
+module.exports = router;
